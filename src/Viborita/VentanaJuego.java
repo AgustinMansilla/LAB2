d4507a26b6724f08b3e35629bb97b6d201405e2b
Agustin <elgasumansilla@gmail.com>
2022-11-15 21:37:50 -0300

    Viborita

---
+++ src/Viborita/VentanaJuego.java
@@ -0,0 +1,22 @@
+package Viborita;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class VentanaJuego extends JFrame {
+
+
+    VentanaJuego(){
+
+        this.add(new PanelJuego());
+        this.setTitle("Viborita");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+}
